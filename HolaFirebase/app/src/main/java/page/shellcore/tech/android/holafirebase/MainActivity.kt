15058424828847d86861cb6aff5b245c36903982
Cesar Morales Garduño <shell.one.core@gmail.com>
2019-10-21 18:46:31 -0500

    Implementación de envío de datos al database de Firebase

--- HolaFirebase/app/src/main/java/page/shellcore/tech/android/holafirebase/MainActivity.kt
+++ HolaFirebase/app/src/main/java/page/shellcore/tech/android/holafirebase/MainActivity.kt
@@ -14,24 +14,38 @@ const val PATH_MESSAGE = "message"
 
 class MainActivity : AppCompatActivity() {
 
+    private val database = FirebaseDatabase.getInstance()
+    private val reference = database.getReference(PATH_START)
+        .child(PATH_MESSAGE)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val database = FirebaseDatabase.getInstance()
-        val reference = database.getReference(PATH_START)
-            .child(PATH_MESSAGE)
+        setOnClickListeners()
+        setFirebaseEventListener()
+    }
 
-        reference.addValueEventListener(object: ValueEventListener {
+    private fun setOnClickListeners() {
+        btnSend.setOnClickListener {
+            reference.setValue(edtMessage.text.toString().trim())
+            edtMessage.setText("")
+        }
+    }
+
+    private fun setFirebaseEventListener() {
+        reference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 txtMessage.text = dataSnapshot.getValue(String::class.java)
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                Toast.makeText(this@MainActivity, "Error al consultar en Firebase", Toast.LENGTH_SHORT)
-                    .show()
+                Toast.makeText(
+                    this@MainActivity,
+                    "Error al consultar en Firebase",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-
         })
     }
 }
