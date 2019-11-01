5964e4dd0a8644a07865e9d2f44c0e8c1cd3c73a
Cesar Morales Garduño <shell.one.core@gmail.com>
2019-11-01 11:43:45 -0600

    Creación del proyecto Ofertas

---
+++ Ofertas/app/src/main/java/com/shellcore/tech/ofertas/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shellcore.tech.ofertas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
