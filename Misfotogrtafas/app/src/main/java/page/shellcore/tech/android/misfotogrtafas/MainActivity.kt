57651070e6430709e9171a337ebf8c3e48cf3a8a
Cesar Morales Garduño <shell.one.core@gmail.com>
2019-12-12 12:59:20 -0600

    Declaración de variables y constantes para el almacenamiento y consulta de las imágenes

--- Misfotogrtafas/app/src/main/java/page/shellcore/tech/android/misfotogrtafas/MainActivity.kt
+++ Misfotogrtafas/app/src/main/java/page/shellcore/tech/android/misfotogrtafas/MainActivity.kt
@@ -1,19 +1,37 @@
 package page.shellcore.tech.android.misfotogrtafas
 
+import android.net.Uri
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.storage.StorageReference
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val RC_GALLERY = 21
+        const val RC_CAMERA = 22
+
+        const val RP_CAMERA = 121
+        const val RP_STORAGE = 122
+
+        const val IMAGE_DIRECTORY = "/MyPhotoApp"
+        const val MY_PHOTO = "my_photo"
+
+        const val PATH_PROFILE = "profile"
+        const val PATH_PHOTO_URL = "photoUrl"
+    }
+
     private lateinit var mStorageReference: StorageReference
     private lateinit var mDatabaseReference: DatabaseReference
+
+    private lateinit var mCurrentPhotoPath: String
+    private lateinit var mPhotoSelectedUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
