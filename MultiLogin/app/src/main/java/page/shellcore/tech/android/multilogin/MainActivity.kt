7fad8c15bd50e748473bb47a4c5ab91586618773
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-01-04 17:09:59 -0600

    Se agregó biblioteca Firebase Auth

--- MultiLogin/app/src/main/java/page/shellcore/tech/android/multilogin/MainActivity.kt
+++ MultiLogin/app/src/main/java/page/shellcore/tech/android/multilogin/MainActivity.kt
@@ -2,11 +2,38 @@ package page.shellcore.tech.android.multilogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
+
+    private val mFirebaseAuth: FirebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    private val mAuthStateListener: FirebaseAuth.AuthStateListener by lazy {
+        FirebaseAuth.AuthStateListener {
+            val user: FirebaseUser? = it.currentUser
+            if (user != null) {
+                // TODO
+            } else {
+                // TODO
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mFirebaseAuth.removeAuthStateListener { mAuthStateListener }
     }
 }
