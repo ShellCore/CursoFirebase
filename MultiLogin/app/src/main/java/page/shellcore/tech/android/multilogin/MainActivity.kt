8813f7be431ade4d212210b758558be074f2894a
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-01-04 17:28:12 -0600

    Se agregó comportamiento para registrar un usuario por usuario y contraseña. Se agregó enlace con servicio Firebase Auth

--- MultiLogin/app/src/main/java/page/shellcore/tech/android/multilogin/MainActivity.kt
+++ MultiLogin/app/src/main/java/page/shellcore/tech/android/multilogin/MainActivity.kt
@@ -1,12 +1,19 @@
 package page.shellcore.tech.android.multilogin
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val RC_SING_IN = 123
+    }
     private val mFirebaseAuth: FirebaseAuth by lazy {
         FirebaseAuth.getInstance()
     }
@@ -17,7 +24,15 @@ class MainActivity : AppCompatActivity() {
             if (user != null) {
                 // TODO
             } else {
-                // TODO
+                startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setIsSmartLockEnabled(false)
+                        .setTosUrl("http://www.google.com")
+                        .setAvailableProviders(arrayListOf(AuthUI.IdpConfig.EmailBuilder().build()))
+                        .build(),
+                    RC_SING_IN
+                )
             }
         }
     }
@@ -25,6 +40,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SING_IN) {
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this, "Bienvenido...", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(this, "Algo falló, intente nuevamente", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 
     override fun onResume() {
