16b8a6d9c1466379f5e8a0e6011fcb7c9b77bec4
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-03-10 12:29:41 -0600

    Configuración de la vista principal de la aplicación

--- SoporteTecnico/app/src/main/java/mx/com/shellcore/android/soportetecnico/RegisterTurnActivity.kt
+++ SoporteTecnico/app/src/main/java/mx/com/shellcore/android/soportetecnico/RegisterTurnActivity.kt
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
-import kotlinx.android.synthetic.main.activity_fullscreen.*
+import kotlinx.android.synthetic.main.activity_registerturn.*
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-class FullscreenActivity : AppCompatActivity() {
+class RegisterTurnActivity : AppCompatActivity() {
     private val mHideHandler = Handler()
     private val mHidePart2Runnable = Runnable {
         // Delayed removal of status and navigation bar
@@ -31,7 +31,7 @@ class FullscreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fullscreen)
+        setContentView(R.layout.activity_registerturn)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
