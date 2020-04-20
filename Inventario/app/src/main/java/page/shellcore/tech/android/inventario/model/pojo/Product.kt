28a6f2ad516968c8b9e50943d3870bfae8befccd
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-04-20 09:10:09 -0500

    Creación de POJO Producto.

---
+++ Inventario/app/src/main/java/page/shellcore/tech/android/inventario/model/pojo/Product.kt
@@ -0,0 +1,36 @@
+package page.shellcore.tech.android.inventario.model.pojo
+
+import com.google.firebase.database.Exclude
+import java.net.IDN
+
+data class Product(
+
+    @Exclude
+    var id: Long,
+    var name: String,
+    var quantity: Int,
+    var photoUrl: String
+) {
+
+    companion object {
+        const val ID = "id"
+        const val NAME = "name"
+        const val QUANTITY = "quantity"
+        const val PHOTO_URL = "photo_url"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Product
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
